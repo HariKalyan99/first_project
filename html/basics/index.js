004dbe4855862e6eb40eb91046e015f352a0766f
Harikalyan <harikalyan99tronics@gmail.com>
2025-07-07 08:11:07 +0530

    basic syntax

---
+++ html/basics/index.js
@@ -0,0 +1,24 @@
+// console.log("Hello world this is my name, first project");
+// console.log('hello world');
+// console.log(`hello world`);
+// console.log('1653783632');
+// console.log(100);
+// console.log("@$@%@%^@%");
+
+// console.log(100 + 100);
+
+// console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
+
+var store = 100 + 100; // 200
+var a = 100 + 300; // 400
+ 
+var b = store; // 200
+
+store = b + a; // 200 + 400
+store = store; // 600
+
+store = a - 400;
+a = store;
+b =  store + a;
+console.log(store, a, b);
+

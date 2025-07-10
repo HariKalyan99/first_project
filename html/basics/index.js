daa96782c9a3f03e5a287567048f071473dce0c9
Harikalyan <harikalyan99tronics@gmail.com>
2025-07-10 08:19:57 +0530

    added node modules

--- html/basics/index.js
+++ html/basics/index.js
@@ -1,63 +1,146 @@
-// console.log("Hello world this is my name, first project");
-// console.log('hello world');
-// console.log(`hello world`);
-// console.log('1653783632');
-// console.log(100);
-// console.log("@$@%@%^@%");
+// // console.log("Hello world this is my name, first project");
+// // console.log('hello world');
+// // console.log(`hello world`);
+// // console.log('1653783632');
+// // console.log(100);
+// // console.log("@$@%@%^@%");
 
-// console.log(100 + 100);
+// // console.log(100 + 100);
 
-// console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
+// // console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
 
-// var store = 100 + 100; // 200
-// var a = 100 + 300; // 400
- 
-// var b = store; // 200
+// // var store = 100 + 100; // 200
+// // var a = 100 + 300; // 400
 
-// store = b + a; // 200 + 400
-// store = store; // 600
+// // var b = store; // 200
 
-// store = a - 400;
-// a = store;
-// b =  store + a;
-// console.log(store, a, b);
+// // store = b + a; // 200 + 400
+// // store = store; // 600
 
+// // store = a - 400;
+// // a = store;
+// // b =  store + a;
+// // console.log(store, a, b);
 
+// // variable declarations and initializations
 
-// variable declarations and initializations
+// // var a;  // var declaration
 
-var a;  // var declaration
+// // a = 90; // var initialization
 
-a = 90; // var initialization
+// // var b = 100; // var initialaization
 
-var b = 100; // var initialaization
+// // var c = true // boolean
 
-var c = true // boolean
+// // a = 100;
 
-a = 100; 
+// // c = 100;
 
-c = 100;
+// // c = false; d = 100;
 
-c = false; d = 100;
+// // console.log(a , b, c);
 
-console.log(a , b, c);
+// // types of storage or variables
 
+// // console.log(a);
 
-// types of storage or variables
+// // var a; // declaration
 
+// // a = 10; //assignment
 
+// // var b = 20; // initialization
 
+// // console.log(a);
 
+// // let b = 10;
 
+// // const c = 10;
 
+// // let a;
 
+// // a = 10;
 
+// // let b = 20;
+// // console.log(b);
 
+// // a = 20;
+// // b = 20; // reinitialize
+// // // c = 20; // never reitializes
 
+// // a = 30; // reinitialize
 
+// // var f;
+// // let u;
+// // const g;
 
+// // g = 10
 
+// // u = 10
 
+// // a = 10; // default
 
+// // let a = 20;
 
+// // console.log(a);
 
+// // // var
+// // let a;
+// // let a
+
+// // console.log(a)
+
+// // data types
+
+// //  number, boolean, string, undefined, null
+
+// // primitive datatypes
+
+// // const a = undefined;
+// // const b = false;
+// // const c = 'string';
+// // const d = 100;
+// // const e = null
+
+// // // non - primitive -> functions, object, arrays
+
+// // function(){
+
+// // }
+
+// // const d = [];
+
+// // const r = {};
+
+// // comparators, logical orperators and strict operators
+
+// const a = 30;
+// // c = 30;
+
+// // const b = c;
+
+// // console.log(a == b); // doesn't check data type
+// // console.log(a === b); // checks data type
+
+// // console.log(a && b);
+// // console.log(a || b);
+// // console.log(a != b);
+
+// // console.log(false || true)
+
+// // console.log(!false)
+
+// // console.log(a && true);
+// // console.log(true && a);
+
+// // console.log(100 && 20)
+
+// console.log(undefined && true);
+// console.log(null && true);
+// console.log(0 && true);
+// console.log('' && true);
+
+// type coersion
+
+// const a = "hello";
+
+// console.log(a + '[]')

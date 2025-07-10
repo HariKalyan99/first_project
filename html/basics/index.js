642533d37087c57cba5d5bb5927ec2c0ff13864a
Harikalyan <harikalyan99tronics@gmail.com>
2025-07-10 06:43:40 +0530

    no comment

--- html/basics/index.js
+++ html/basics/index.js
@@ -9,16 +9,55 @@
 
 // console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
 
-var store = 100 + 100; // 200
-var a = 100 + 300; // 400
+// var store = 100 + 100; // 200
+// var a = 100 + 300; // 400
  
-var b = store; // 200
+// var b = store; // 200
 
-store = b + a; // 200 + 400
-store = store; // 600
+// store = b + a; // 200 + 400
+// store = store; // 600
 
-store = a - 400;
-a = store;
-b =  store + a;
-console.log(store, a, b);
+// store = a - 400;
+// a = store;
+// b =  store + a;
+// console.log(store, a, b);
+
+
+
+// variable declarations and initializations
+
+var a;  // var declaration
+
+a = 90; // var initialization
+
+var b = 100; // var initialaization
+
+var c = true // boolean
+
+a = 100; 
+
+c = 100;
+
+c = false; d = 100;
+
+console.log(a , b, c);
+
+
+// types of storage or variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

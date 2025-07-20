b2c29bf19f4dd27423b0f2614982cba5a15a3aab
Harikalyan <harikalyan99tronics@gmail.com>
2025-07-20 18:31:12 +0530

    function

--- html/basics/index.js
+++ html/basics/index.js
@@ -1,329 +1,504 @@
-// // console.log("Hello world this is my name, first project");
-// // console.log('hello world');
-// // console.log(`hello world`);
-// // console.log('1653783632');
-// // console.log(100);
-// // console.log("@$@%@%^@%");
+// // // // // console.log("Hello world this is my name, first project");
+// // // // // console.log('hello world');
+// // // // // console.log(`hello world`);
+// // // // // console.log('1653783632');
+// // // // // console.log(100);
+// // // // // console.log("@$@%@%^@%");
 
-// // console.log(100 + 100);
+// // // // // console.log(100 + 100);
 
-// // console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
+// // // // // console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
 
-// // var store = 100 + 100; // 200
-// // var a = 100 + 300; // 400
+// // // // // var store = 100 + 100; // 200
+// // // // // var a = 100 + 300; // 400
 
-// // var b = store; // 200
+// // // // // var b = store; // 200
 
-// // store = b + a; // 200 + 400
-// // store = store; // 600
+// // // // // store = b + a; // 200 + 400
+// // // // // store = store; // 600
 
-// // store = a - 400;
-// // a = store;
-// // b =  store + a;
-// // console.log(store, a, b);
+// // // // // store = a - 400;
+// // // // // a = store;
+// // // // // b =  store + a;
+// // // // // console.log(store, a, b);
 
-// // variable declarations and initializations
+// // // // // variable declarations and initializations
 
-// // var a;  // var declaration
+// // // // // var a;  // var declaration
 
-// // a = 90; // var initialization
+// // // // // a = 90; // var initialization
 
-// // var b = 100; // var initialaization
+// // // // // var b = 100; // var initialaization
 
-// // var c = true // boolean
+// // // // // var c = true // boolean
 
-// // a = 100;
+// // // // // a = 100;
 
-// // c = 100;
+// // // // // c = 100;
 
-// // c = false; d = 100;
+// // // // // c = false; d = 100;
 
-// // console.log(a , b, c);
+// // // // // console.log(a , b, c);
 
-// // types of storage or variables
+// // // // // types of storage or variables
 
-// // console.log(a);
+// // // // // console.log(a);
 
-// // var a; // declaration
+// // // // // var a; // declaration
 
-// // a = 10; //assignment
+// // // // // a = 10; //assignment
 
-// // var b = 20; // initialization
+// // // // // var b = 20; // initialization
 
-// // console.log(a);
+// // // // // console.log(a);
 
-// // let b = 10;
+// // // // // let b = 10;
 
-// // const c = 10;
+// // // // // const c = 10;
 
-// // let a;
+// // // // // let a;
 
-// // a = 10;
+// // // // // a = 10;
 
-// // let b = 20;
-// // console.log(b);
+// // // // // let b = 20;
+// // // // // console.log(b);
 
-// // a = 20;
-// // b = 20; // reinitialize
-// // // c = 20; // never reitializes
+// // // // // a = 20;
+// // // // // b = 20; // reinitialize
+// // // // // // c = 20; // never reitializes
 
-// // a = 30; // reinitialize
+// // // // // a = 30; // reinitialize
 
-// // var f;
-// // let u;
-// // const g;
+// // // // // var f;
+// // // // // let u;
+// // // // // const g;
 
-// // g = 10
+// // // // // g = 10
 
-// // u = 10
+// // // // // u = 10
 
-// // a = 10; // default
+// // // // // a = 10; // default
 
-// // let a = 20;
+// // // // // let a = 20;
 
-// // console.log(a);
+// // // // // console.log(a);
 
-// // // var
-// // let a;
-// // let a
+// // // // // // var
+// // // // // let a;
+// // // // // let a
 
-// // console.log(a)
+// // // // // console.log(a)
 
-// // data types
+// // // // // data types
 
-// //  number, boolean, string, undefined, null
+// // // // //  number, boolean, string, undefined, null
 
-// // primitive datatypes
+// // // // // primitive datatypes
 
-// // const a = undefined;
-// // const b = false;
-// // const c = 'string';
-// // const d = 100;
-// // const e = null
+// // // // // const a = undefined;
+// // // // // const b = false;
+// // // // // const c = 'string';
+// // // // // const d = 100;
+// // // // // const e = null
 
-// // // non - primitive -> functions, object, arrays
+// // // // // // non - primitive -> functions, object, arrays
 
-// // function(){
+// // // // // function(){
 
+// // // // // }
+
+// // // // // const d = [];
+
+// // // // // const r = {};
+
+// // // // // comparators, logical orperators and strict operators
+
+// // // // const a = 30;
+// // // // // c = 30;
+
+// // // // // const b = c;
+
+// // // // // console.log(a == b); // doesn't check data type
+// // // // // console.log(a === b); // checks data type
+
+// // // // // console.log(a && b);
+// // // // // console.log(a || b);
+// // // // // console.log(a != b);
+
+// // // // // console.log(false || true)
+
+// // // // // console.log(!false)
+
+// // // // // console.log(a && true);
+// // // // // console.log(true && a);
+
+// // // // // console.log(100 && 20)
+
+// // // // console.log(undefined && true);
+// // // // console.log(null && true);
+// // // // console.log(0 && true);
+// // // // console.log('' && true);
+
+// // // // type coersion
+
+// // // // const a = "hello";
+
+// // // // console.log(a + '[]')
+
+// // // // if else condition
+
+// // // // console.log(100 + 20 != 120);
+
+// // // // const val = "razor";
+
+// // // // if (!(false === null)) {
+// // // //   console.log("yes it is true");
+// // // // } else {
+// // // //   console.log("no it is false");
+// // // // }
+
+// // // // console.log(typeof (val + 100) === "string")
+
+// // // //  == , ===
+
+// // // // var a = 100;
+// // // // var b = 0;
+
+// // // // if (50 + 80 === a + 20) {
+// // // //   console.log("it's true @1");
+
+// // // //   if (typeof a !== "number") {
+// // // //     console.log("yes it is a number");
+// // // //   } else {
+// // // //     console.log("no it is not a number");
+// // // //   }
+// // // // } else if (30 + 90 === a) {
+// // // //   console.log("it's true @2");
+// // // // } else if (30 + 70 !== a + b) {
+// // // //   console.log("it's true @3");
+
+// // // //   if (typeof a !== "number") {
+// // // //     console.log("yes it is a number");
+// // // //   } else {
+// // // //     console.log("no it is not a number");
+// // // //   }
+// // // // } else {
+// // // //   console.log("it's false");
+// // // // }
+
+// // // // console.log(typeof 1)
+
+// // // // console.log("number" === 'number')
+
+// // // // Loops -> loops are those which occur for multiple execution of the described code.
+// // // // for(initialization, condition, updater) {
+
+// // // // }
+
+// // // // for loop
+
+// // // // let arr = [
+// // // //   "hello this is my new text" , //0
+// // // //    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus voluptate in nesciunt!" , //1
+// // // //   "Illum, doloribus deleniti dolorem, et amet ipsum adipisci quos, quae magni saepe nostrum modi! Quaerat, delectus qui!", //2
+// // // // ]
+
+// // // // for(i = -10; i<=-5; i--){ // (i = 0) (0 < 10) (i + 1)/(i++) => i = 1
+// // // //   console.log(i)
+// // // // }
+
+// // // // var i = 1;
+
+// // // // for(i; i<=11; i+=5){ // (i = 0) (0 < 10) (i + 1)/(i++) => i = 1
+// // // //   console.log(i)
+// // // // }
+// // // // console.log(i+=10)
+
+// // // // i = 10 + 15 => i+=15
+
+// // // // indexing
+
+// // // // 0 -> 1 --> 2 ---> 3
+// // // // first iteration // (i = 0) (0 < 10) (i + 1)/(i++) => i = 1
+// // // // second iteration // (i = 1) (1 < 10) (1 + 1)/(i++) => i = 2
+// // // // third iteration // (i = 2) (2 < 10) (1 + 1)/(i++) => i = 2
+// // // // .
+// // // // .
+// // // // .
+// // // // .
+
+// // // // var b = 10;
+// // // // b+=1;
+
+// // // // console.log(b + 1);
+
+// // // // loop starts from 5 -> (excluding)500 and reverse it
+// // // // now start from 498 while reversing it?
+// // // // skip 5 on each iteration?
+
+// // // // loop starts from 2 -> (excluding)10 and reverse it
+// // // // now start from 8 while reversing it?
+// // // // skip 3 on each iteration?
+// // // // print "the number I am iterating is: " + val on every iteration
+
+// // // // non primitive data types
+
+// // // // function gokul(){ //declaration
+// // // //   // code exec start
+// // // //   console.log("Good evening");
+// // // //   // code exec end
+// // // // }
+
+// // // // gokul(); // fn call
+
+// // // // function greetings(parameters) { // declaration
+
+// // // // }
+
+// // // // greetings(arguements); // call
+
+// // // // function greetings
+
+// // // // case ->
+
+// // // // greetingsFunction -> camelcase letters
+// // // // Greetingsfunction -> pascal/title case letters
+// // // // greetings-function -> kebab case letters
+// // // // greetings_function -> snake case letters
+
+// // // function asdkasdka(valalalala1, val2, val3, val4, val5, val6){
+// // //   console.log(typeof valalalala1, "value: ", valalalala1);
+// // //   console.log(typeof val2, "value: ", val2);
+// // //   console.log(typeof val3, "value: ", val3);
+// // //   console.log(typeof val4, "value: ", val4);
+// // //   console.log(typeof val5, "value: ", val5);
+// // //   console.log(typeof val6, "value: ", val6);
+
+// // //   //
+
+// // //   console.log(valalalala1, val2, val3, val4, val5, val6);
+
+// // // }
+
+// // // asdkasdka("10", true, 10, undefined, null, "good morning");
+
+// // // function greetings(val){
+// // //   console.log("its " + val + " o clock in the morning")
+// // // }
+
+// // // greetings(10)
+
+// // // // greet according to time (24h format)
+// // // // pass arguments, based on those greet accordingly
+// // // // ex 6 -> "good morning!"
+// // // // ex 15 -> "good afternoon!"
+// // // // ex 18 -> "good evening!"
+// // // // ex 21 -> "good night!"
+
+// // // // 1-12 as "good morning!"
+// // // // 13-16 as "good afternoon!"
+// // // // 16-19 as "good evening!"
+// // // // 19-23 as "good night!"
+
+// // // // countdown timer
+// // // // create a function to count down from 100 - 0;
+
+// // // // non - primitive data types
+
+// // // // es 5 ecma script 5 2015
+// // // function hello1(){
+// // //   console.log("Hello1");
+// // // }
+
+// // // // es 6+ es 22 2016
+// // // const hello2 = () => {
+// // //   console.log("Hello2");
+// // // }
+
+// // // hello1();
+// // // hello2();
+// // // value = null
+// // // console.log(typeof null && value === null)
+
+
+// // // console.log(typeof {} === typeof null)
+
+
+// // // function counter() {
+// // //   for (let i = 1; i <= 10; i++) {
+// // //     console.log(i);
+// // //   }
+// // // }
+
+// // // // Call the function
+// // // counter();
+
+
+// // // function fullName(lastName, brotherName){
+// // //   // console.log(brotherName)
+// // //   console.log("My name is Gokul " + lastName);
+// // //   console.log(`My name is Gokul ${lastName}`);
+// // //   return
+// // // }
+
+
+// // // fullName("Gurumurthy", "ashwin")
+
+
+
+// // function fullName(lastName){
+// //   const firstName = "Gokul"; // gokul
+// //   const fullName = firstName.concat(lastName); // gokul....
+// //   console.log('My name is ' + fullName); // true
+// //   console.log("My name is Gokul " + lastName); // true
+// //   console.log(`My name is Gokul ${lastName}`); // string literals // true
+// //   return `My name is ${fullName}` // true
 // // }
 
-// // const d = [];
 
-// // const r = {};
+// // // const myName = fullName("Gurumurthy");
 
-// // comparators, logical orperators and strict operators
 
-// const a = 30;
-// // c = 30;
+// // console.log(fullName("Gurumurthy"))
 
-// // const b = c;
 
-// // console.log(a == b); // doesn't check data type
-// // console.log(a === b); // checks data type
 
-// // console.log(a && b);
-// // console.log(a || b);
-// // console.log(a != b);
 
-// // console.log(false || true)
-
-// // console.log(!false)
-
-// // console.log(a && true);
-// // console.log(true && a);
-
-// // console.log(100 && 20)
-
-// console.log(undefined && true);
-// console.log(null && true);
-// console.log(0 && true);
-// console.log('' && true);
-
-// type coersion
-
-// const a = "hello";
-
-// console.log(a + '[]')
-
-// if else condition
-
-// console.log(100 + 20 != 120);
-
-// const val = "razor";
-
-// if (!(false === null)) {
-//   console.log("yes it is true");
-// } else {
-//   console.log("no it is false");
+// // let outPut;
+// function name() {
+//   // declare 'output' as a variable;
+//   // use for loop for countdown from 10 - 1;
+//   // 
 // }
 
-// console.log(typeof (val + 100) === "string")
 
-//  == , ===
+// function countUp(){
+//   let time = 0;
+//   for(let i = 0; i<=10; i++){ // 0, 1, 2 +1 , 3, 4+ 1, 5, 6+1, ....10 + 1
+//  console.log(i); 
 
-// var a = 100;
-// var b = 0;
-
-// if (50 + 80 === a + 20) {
-//   console.log("it's true @1");
-
-//   if (typeof a !== "number") {
-//     console.log("yes it is a number");
-//   } else {
-//     console.log("no it is not a number");
+//     time = i + 1; 
 //   }
-// } else if (30 + 90 === a) {
-//   console.log("it's true @2");
-// } else if (30 + 70 !== a + b) {
-//   console.log("it's true @3");
+//   console.log(time); // 11
+//   return time;
+// }
 
-//   if (typeof a !== "number") {
-//     console.log("yes it is a number");
-//   } else {
-//     console.log("no it is not a number");
+// // console.log(countUp());
+
+
+
+// // // `my name is hari ${}`
+
+
+
+
+
+
+
+
+// // function print100(value){
+// //   return value;
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function printEven(){
+//   for(let i = 1; i<=10; i++){ // 0, 1, 2 +1 , 3, 4+ 1, 5, 6+1, ....10 + 1
+//     if(i%2 === 0){
+//       console.log(`${i} is even number`)
+//     } 
 //   }
-// } else {
-//   console.log("it's false");
 // }
 
-
-
-// console.log(typeof 1)
-
-// console.log("number" === 'number')
-
-
-// Loops -> loops are those which occur for multiple execution of the described code.
-// for(initialization, condition, updater) {
-
-
-// }
-
-// for loop
-
-// let arr = [
-//   "hello this is my new text" , //0
-//    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus voluptate in nesciunt!" , //1
-//   "Illum, doloribus deleniti dolorem, et amet ipsum adipisci quos, quae magni saepe nostrum modi! Quaerat, delectus qui!", //2
-// ]
-
-
-// for(i = -10; i<=-5; i--){ // (i = 0) (0 < 10) (i + 1)/(i++) => i = 1
-//   console.log(i)
-// }
-
-// var i = 1;
-
-
-// for(i; i<=11; i+=5){ // (i = 0) (0 < 10) (i + 1)/(i++) => i = 1
-//   console.log(i)
-// }
-// console.log(i+=10)
-
-// i = 10 + 15 => i+=15
-
-// indexing
-
-
-// 0 -> 1 --> 2 ---> 3
-// first iteration // (i = 0) (0 < 10) (i + 1)/(i++) => i = 1
-// second iteration // (i = 1) (1 < 10) (1 + 1)/(i++) => i = 2
-// third iteration // (i = 2) (2 < 10) (1 + 1)/(i++) => i = 2
-// .
-// . 
-// .
-// .
-
-
-// var b = 10;
-// b+=1;
-
-// console.log(b + 1);
-
-
-// loop starts from 5 -> (excluding)500 and reverse it 
-// now start from 498 while reversing it?
-// skip 5 on each iteration?
-
-
-// loop starts from 2 -> (excluding)10 and reverse it 
-// now start from 8 while reversing it?
-// skip 3 on each iteration?
-// print "the number I am iterating is: " + val on every iteration
-
-
-
-// non primitive data types
-
-
-// function gokul(){ //declaration
-//   // code exec start
-//   console.log("Good evening");
-//   // code exec end
-// }
-
-// gokul(); // fn call
-
-
-
-// function greetings(parameters) { // declaration
-  
-// }
-
-// greetings(arguements); // call
-
-
-// function greetings
-
-// case -> 
-
-
-// greetingsFunction -> camelcase letters
-// Greetingsfunction -> pascal/title case letters
-// greetings-function -> kebab case letters
-// greetings_function -> snake case letters
+// printEven()
 
 
 
 
-function asdkasdka(valalalala1, val2, val3, val4, val5, val6){
-  console.log(typeof valalalala1, "value: ", valalalala1);
-  console.log(typeof val2, "value: ", val2);
-  console.log(typeof val3, "value: ", val3);
-  console.log(typeof val4, "value: ", val4);
-  console.log(typeof val5, "value: ", val5);
-  console.log(typeof val6, "value: ", val6);
-
-  // 
-
-  console.log(valalalala1, val2, val3, val4, val5, val6);
-
-}
-
-asdkasdka("10", true, 10, undefined, null, "good morning");
 
 
 
-function greetings(val){
-  console.log("its " + val + " o clock in the morning")
-}
-
-greetings(10) 
-
-// greet according to time (24h format)
-// pass arguments, based on those greet accordingly
-// ex 6 -> "good morning!"
-// ex 15 -> "good afternoon!"
-// ex 18 -> "good evening!"
-// ex 21 -> "good night!"
-
-
-// 1-12 as "good morning!"
-// 13-16 as "good afternoon!"
-// 16-19 as "good evening!"
-// 19-23 as "good night!"
 
 
 
-// countdown timer
-// create a function to count down from 100 - 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
